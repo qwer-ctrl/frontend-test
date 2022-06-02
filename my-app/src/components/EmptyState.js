a6e495b5b375e3eeb8375efa9f3b22715c4ed93d
Emma Högberg <emmahogberg88@gmail.com>
2022-06-02 21:47:40 +0200

    Add emptystate component

---
+++ my-app/src/components/EmptyState.js
@@ -0,0 +1,29 @@
+import React from "react"
+import styled from 'styled-components/macro'
+
+
+const EmptyState = () => {
+
+    return (
+        <>
+        <EmptyStateContainer>
+        <p>EmptyState</p>
+        <button>Add new program</button>
+        </EmptyStateContainer>
+        </>
+    )
+}
+
+export default EmptyState
+
+const EmptyStateContainer = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: white;
+`
