da493726edc29ad2a4d044ad81e1bbcb9a35c94d
anki palviainen <anki.palviainen@gmail.com>
2022-06-13 14:11:59 +0300

    add heroku link

--- my-app/src/utils/utils.js
+++ my-app/src/utils/utils.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://git.heroku.com/flexnjoy.git"
 
-//"https://git.heroku.com/flexnjoy.git"
+//"http://localhost:8080"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
