bcaef5729679ac081cd05ce62a7f989de03f1b68
anki palviainen <anki.palviainen@gmail.com>
2022-06-13 09:01:34 +0300

    for netlify

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import Login from './pages/Login'
-import MyPage from './pages/MyPage'
+//import MyPage from './pages/MyPage'
 import AddProgram from './pages/AddProgram'
 import SingleProgram from './pages/SingleProgram'
 import NotFound from './pages/NotFound'
@@ -39,10 +39,10 @@ export const App = () => {
 		<Provider store={store}>
 			<BrowserRouter>
 				<Routes>
-					<Route path='/login' element={<Login />}></Route>
+					{/* <Route path='/login' element={<Login />}></Route> */}
 					<Route path='/addprogram/:programId' element={<AddProgram />}></Route>
 					<Route path='/singleprogram/:programId' element={<SingleProgram />}></Route>
-					<Route path='/' element={<MyPage />}></Route>
+					<Route path='/' element={<Login />}></Route>
 					<Route path='*' element={<NotFound />}></Route>
 				</Routes>
 			</BrowserRouter>
