60fe1818a8013d17cb0e1b69f6cc7248eb5d64d8
anki palviainen <anki.palviainen@gmail.com>
2022-06-13 09:53:19 +0300

    localhost

--- my-app/src/utils/utils.js
+++ my-app/src/utils/utils.js
@@ -1,3 +1,5 @@
-const BASE_URL = "https://git.heroku.com/flexnjoy.git"
+const BASE_URL = "http//localhost:8080"
+
+//"https://git.heroku.com/flexnjoy.git"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
